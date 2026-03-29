eab5edbec10c435d55893a9d45d25fd6939e0303
Ashutosh1783 <ag3621559@gmail.com>
2026-03-29 16:17:37 +0530

    Add files via upload

---
+++ movie_recommender_app (2).py
@@ -0,0 +1,31 @@
+
+import streamlit as st
+import pickle
+
+mv = pickle.load(open('movies.pkl','rb'))
+sim = pickle.load(open('similarity.pkl','rb'))
+
+def recommend(movie):
+    movie = movie.lower()
+
+    if movie not in mv['title'].str.lower().values:
+        return ["Movie not found"]
+
+    index = mv[mv['title'].str.lower() == movie].index[0]
+    distances = sim[index]
+
+    movies_list = sorted(list(enumerate(distances)),
+                         reverse=True,
+                         key=lambda x:x[1])[1:6]
+
+    return [mv.iloc[i[0]].title for i in movies_list]
+
+st.title("🎬 Movie Recommendation System")
+
+selected_movie = st.selectbox("Select a movie", mv['title'].values)
+
+if st.button("Recommend"):
+    recommendations = recommend(selected_movie)
+
+    for movie in recommendations:
+        st.write(movie)
